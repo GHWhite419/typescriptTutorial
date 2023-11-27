eb52e276350c0e88d638869a1b59e280aa898e5f
Galen White <galenhwhite419@gmail.com>
2023-11-26 20:47:55 -0500

    Initial commit

---
+++ lesson01/src/main.ts
@@ -0,0 +1,93 @@
+// Type Aliases
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = (string | number)[];
+
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: stringOrNumberArray;
+};
+
+type userId = stringOrNumber;
+
+// Literal types
+let myName: "Galen";
+
+let userName: "Galen" | "Jon" | "Arya";
+userName = "Arya";
+
+// Functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg("Howdy!");
+logMsg(add(2, 3));
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+type mathFunction = (a: number, b: number) => number;
+// interface mathFunction {
+// (a: number, b: number): number;
+// }
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+// Optional Parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+// Default param value
+const sumAll = (a: number = 10, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(undefined, 3));
+
+// Rest parameters
+const total = (a: number, ...nums: number[]): number => {
+  return a + nums.reduce((prev, curr) => prev + curr);
+};
+
+logMsg(total(10, 2, 3));
+
+const createError = (errMsg: string) => {
+  throw new Error(errMsg);
+};
+
+const infinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) break;
+  }
+};
+
+// Custom type guard
+const isNumber = (value: any): boolean => {
+  return typeof value === "number" ? true : false;
+};
+
+// Use of 'never' type
+const numberOrString = (value: number | string): string => {
+  if (typeof value === "string") return "string";
+  if (isNumber(value)) return "number";
+  return createError("This should never happen");
+};
